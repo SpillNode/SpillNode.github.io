01937e273a802241d195b327f1c5f24b588e368b
BamseMoms <30778293+BamseMoms@users.noreply.github.com>
2021-08-31 21:48:16 +0200

    Nettside V3

--- README.md
+++
@@ -1,1 +0,0 @@
-# SpillNode.github.io
--- assets/js/year.js
+++
@@ -1,5 +0,0 @@
-var paragraph = document.getElementById("year");
-var d = new Date();
-var n = d.getFullYear();
-var text = document.createTextNode(n);
-paragraph.appendChild(text);
